388b86d26e7f996802413d6083ec0334f4ffe14a
DanielBenjamin12 <benjamindaniel711@gmail.com>
2025-07-05 16:20:37 -0400

    Creacion de mi primer componente en Vue

---
+++ src/components/Contador.vue
@@ -0,0 +1,36 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <p>{{contador }}</p>
+
+  <button @click="incrementar()">
+    incrementar
+  </button>
+    <button @click="decrementar()">
+    decrementar
+  </button>
+</template>
+
+<script setup>
+
+  import { ref } from 'vue';
+
+  const contador =  ref(0)
+  console.log(contador)
+
+  const incrementar = () =>{
+    contador.value++
+  }
+
+  const decrementar = () => {
+    contador.value--
+  }
+
+</script>
+
+<style scoped>
+
+p{
+  color: lightblue;
+}
+
+</style>
